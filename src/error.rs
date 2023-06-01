d909d175805bec5c6d5cdaef905d1714d138994e
ImportWU <1248951273@qq.com>
2023-06-01 12:22:45 +0800

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -7,14 +7,16 @@ use std::{
 #[derive(Debug, PartialEq)]
 pub enum Error<T> {
     Unexpected(T),
-    Eoi
+    Eoi,
+    Custom(String)
 }
 
 impl<T: fmt::Display> fmt::Display for Error<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
            Self::Unexpected(t) =>  write!(f, "unexpected {}", t),
-           Self::Eoi => write!(f, "end of input")
+           Self::Eoi => write!(f, "end of input"),
+           Self::Custom(msg) -> write!(f, msg)
         }        
     }
 }
