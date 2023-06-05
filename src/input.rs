4208c151ed6dbe724c38d6a9e269aeb910d9f29e
ImportWU <1248951273@qq.com>
2023-06-05 11:58:02 +0800

    Update input.rs

--- src/input.rs
+++ src/input.rs
@@ -1,7 +1,10 @@
 use std::{
     str::Chars, 
     slice::Iter, 
-    iter::Copied
+    iter::{
+        Copied,
+        Cloned
+    }
 };
 
 use crate::{
@@ -76,6 +79,31 @@ impl<'a> Input for &'a [u8] {
 
     fn tokens(&self) -> Self::Tokens {
         self.iter().copied()
+    }
+}
+
+impl<'a, T: Clone> Input for &'a [T] {
+    type Token = T;
+    type Tokens = Cloned<Iter<'a, T>>
+    
+    fn next(&mut self) -> Option<Self::Token> {
+        let mut iter = self.iter();
+        let item = iter.cloned().next()?;
+        *self = iter.as_slice();
+        Some(item)
+    }
+    
+    fn peek(&mut self) -> Option<Self::Token> {
+        self.iter().cloned().next()
+    }
+    
+    fn diff(&self, other: &Self) -> Self {
+        let offset = other.as_ptr() as usize - self.as_ptr() as usize;
+        &self[..offset]
+    }
+    
+    fn tokens(&self) -> Self::Tokens {
+        self.iter().cloned()
     }
 }
 
