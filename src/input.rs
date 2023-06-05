934ccd08927624a98bf1f82be067ac56c8a28d56
ImportWU <1248951273@qq.com>
2023-06-05 12:04:07 +0800

    Update input.rs

--- src/input.rs
+++ src/input.rs
@@ -57,34 +57,34 @@ impl<'a> Input for &'a str {
     }
 }
 
-impl<'a> Input for &'a [u8] {
-    type Token = u8;
-    type Tokens = Copied<Iter<'a, u8>>;
+// impl<'a> Input for &'a [u8] {
+//     type Token = u8;
+//     type Tokens = Copied<Iter<'a, u8>>;
 
-    fn next(&mut self) -> Option<Self::Token> {
-        let mut iter = self.iter();
-        let item = *iter.next()?;
-        *self = iter.as_slice();
-        Some(item)
-    }
+//     fn next(&mut self) -> Option<Self::Token> {
+//         let mut iter = self.iter();
+//         let item = *iter.next()?;
+//         *self = iter.as_slice();
+//         Some(item)
+//     }
 
-    fn peek(&mut self) -> Option<Self::Token> {
-        self.iter().copied().next()
-    }
+//     fn peek(&mut self) -> Option<Self::Token> {
+//         self.iter().copied().next()
+//     }
 
-    fn diff(&self, other: &Self) -> Self {
-        let offset = other.as_ptr() as usize - self.as_ptr() as usize;
-        &self[..offset]
-    }
+//     fn diff(&self, other: &Self) -> Self {
+//         let offset = other.as_ptr() as usize - self.as_ptr() as usize;
+//         &self[..offset]
+//     }
 
-    fn tokens(&self) -> Self::Tokens {
-        self.iter().copied()
-    }
-}
+//     fn tokens(&self) -> Self::Tokens {
+//         self.iter().copied()
+//     }
+// }
 
 impl<'a, T: Clone> Input for &'a [T] {
     type Token = T;
-    type Tokens = Cloned<Iter<'a, T>>
+    type Tokens = Cloned<Iter<'a, T>>;
     
     fn next(&mut self) -> Option<Self::Token> {
         let mut iter = self.iter();
