fb8844898dfdb6e557ed2cd97bf77ecfad1c68b8
ImportWU <1248951273@qq.com>
2023-06-25 17:01:18 +0800

    Update json.rs

--- examples/json.rs
+++ examples/json.rs
@@ -29,16 +29,25 @@ use rtor::{
 };
 
 fn main() {
-
     let s = r#"
-        {
-            "number": [1, 2, 2e2, 3e-2, 4.0],
-            "value": [true, false, null, [1, 2, 3], "\"\u123afsawda"],
-            "obj": {
-                "v": 2,
-                "x": null
-            }
-        }
+    {
+        "ulozec": [
+            [
+                [-780982275, true],
+                -1071421524
+            ],
+            {
+                "hyghokilpgf": -694363646.9191926,
+                "ialcqh": 1377465553.898079,
+                "oozscfql": "spWyychnqYA5R"
+            },
+            [1959856920, true, false],
+            true
+        ],
+        "yvfuqw": "V",
+        "dswcppi": "4I2xOR_q",
+        "ytgqmlpld": "Y7wd"
+    }
     "#;
     
     let json_value = token(json).andl(token(eof)).parse(s);
@@ -71,10 +80,7 @@ fn json(input: &str) -> ParseResult<JsonValue, &str> {
 fn json_object(input: &str) -> ParseResult<JsonValue, &str> {
     between(
         char('{'), 
-        sepby(
-            pair(token(key), token(char(':')),  token(json)), 
-            token(char(','))
-        ), 
+        sepby(pair(token(key), token(char(':')),  token(json)), token(char(','))), 
         token(char('}'))
     )
     .map(|members| JsonValue::Object(HashMap::from_iter(members)))
@@ -92,10 +98,8 @@ fn json_array(input: &str) -> ParseResult<JsonValue, &str> {
 }
 
 fn key(input: &str) -> ParseResult<String, &str> {
-    let escape =  oneof("\"\\/bfnrt").ignore()
-        .or(char('u').andr(skip(hex, 4)));
-    let character = char('\\').andr(escape)
-        .or(not(char('"')).andl(anychar));
+    let escape =  oneof("\"\\/bfnrt").or(char('u').andl(skip(hex, 4)));
+    let character = char('\\').andr(escape).or(not(char('"')).andl(anychar));
     between(
         char('"'), 
         recognize(skip_many(character)).map(|i: &str| i.to_owned()),
@@ -105,9 +109,7 @@ fn key(input: &str) -> ParseResult<String, &str> {
 }
 
 fn json_number(input: &str) -> ParseResult<JsonValue, &str> { 
-    let exponent = char('E').or(char('e'))
-        .andr(opt(char('+').or(char('-'))))
-        .andr(skip_many1(digit));
+    let exponent = char('E').or(char('e')).andr(opt(char('+').or(char('-')))).andr(skip_many1(digit));
     let fraction = char('.').andr(skip_many1(digit));
     let integer = char('0').or(sat(|ch| matches!(ch, '1'..='9')).andl(skip_many(digit)));
     recognize(opt(char('-')).andr(integer).andr(opt(fraction)).andr(opt(exponent)))
